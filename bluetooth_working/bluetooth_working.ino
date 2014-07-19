fecaf997baae1ee5fa2750e33359b8027a2855ab
Michael Schmitt <schmiddim@gmx.at>
2014-07-19 10:13:58 +0200

    bt code - moved to Analog Pins

--- bluetooth_working/bluetooth_working.ino
+++ bluetooth_working/bluetooth_working.ino
@@ -24,20 +24,22 @@ This works on Digital Pins!
 
 HC-05 GND --- Arduino GND Pin
 HC-05 VCC (5V) --- Arduino 5V
-HC-05 TX --- Arduino Pin 10 (soft RX)
-HC-05 RX --- Arduino Pin11 (soft TX)
-HC-05 Key (PIN 34) --- Arduino Pin 9
+HC-05 TX --- Arduino Pin A0 (soft RX)
+HC-05 RX --- Arduino Pin A1 (soft TX)
+HC-05 Key (PIN 34) --- Arduino 5V
 
 
 Beeper Wiring 12
 */
+#define BT_RX A1
+#define BT_TX A0
 
 
 #include <SoftwareSerial.h>
  
-SoftwareSerial BTSerial(10, 11); // RX | TX
+SoftwareSerial BTSerial(BT_RX, BT_TX); // RX | TX
 const int buzzer = 8;       //the buzzer
-const int led=12;  
+const int led=A2;  
 
 
 String inputString = "";         // a string to hold incoming data
@@ -49,7 +51,9 @@ void setup()
   
   
   pinMode(8, OUTPUT);  // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode
-  digitalWrite(9, HIGH);
+  
+  
+  digitalWrite(BT_KEY, HIGH);
   Serial.begin(9600);
   Serial.println("Enter AT commands:");
   BTSerial.begin(9600);  // HC-05 default speed in AT command more
